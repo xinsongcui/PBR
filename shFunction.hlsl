2891216be10155181f9313f213851649d9d6c7b9
Xinsong Cui <43189418+xinsongcui@users.noreply.github.com>
2019-08-01 17:15:45 +0800

    Add files via upload

---
+++ shFunction.hlsl
@@ -0,0 +1,8 @@
+﻿// include "Packages/com.unity.render-pipelines.lightweight/ShaderLibrary/Lighting.hlsl"
+
+void LWRPshFunction_float (float3 normal, out half3 sh)
+{   
+   sh = SampleSH(half3(normal));
+   
+}
+    
